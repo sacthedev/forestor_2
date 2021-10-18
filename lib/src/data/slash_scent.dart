9b4f10ec5751cfc96bb63fff40d86bc59e4a94a3
ZRELOLERZ <batmanbane4@gmail.com>
2021-10-17 21:57:06 -0400

    - added slash scent key

---
+++ lib/src/data/slash_scent.dart
@@ -0,0 +1,79 @@
+Map<String, dynamic> slashScent = {
+  "slashScent": {
+    "text": "slash scent",
+    "subCharacteristics": [
+      {
+        "cucumberLike": {
+          "text": "cucumber-like",
+          "treeIDs": [1, 3, 18, 51, 52]
+        }
+      },
+      {
+        "linseedOilLike": {
+          "text": "linseed oil-like",
+          "treeIDs": [19, 26, 27, 28, 38, 39]
+        }
+      },
+      {
+        "ranaleanAromatic": {
+          "text": "ranalean-aromatic (Silverballi-like)",
+          "treeIDs": [5, 16, 42, 48, 49, 50]
+        }
+      },
+      {
+        "sweet": {
+          "text": "sweet",
+          "treeIDs": [
+            6,
+            7,
+            20,
+            22,
+            36,
+            41,
+            43,
+            45,
+            57,
+            59,
+            62,
+            64,
+            67,
+            74,
+            80,
+            82,
+            83
+          ]
+        }
+      },
+      {
+        "sour": {
+          "text": "sour",
+          "treeIDs": [29]
+        }
+      },
+      {
+        "tobaccoLike": {
+          "text": "tobacco-like",
+          "treeIDs": [40, 71]
+        }
+      },
+      {
+        "cedarLike": {
+          "text": "cedar-like",
+          "treeIDs": [15]
+        }
+      },
+      {
+        "garlicLike": {
+          "text": "garlic-like",
+          "treeIDs": [15]
+        }
+      },
+      {
+        "bitter": {
+          "text": "bitter",
+          "treeIDs": [62]
+        }
+      }
+    ]
+  }
+};
--- lib/src/data/tree_key.dart
+++ lib/src/data/tree_key.dart
@@ -1,11 +1,13 @@
 import 'package:forestor_2/src/data/bark_surface_color.dart';
 import 'package:forestor_2/src/data/base_of_tree.dart';
 import 'package:forestor_2/src/data/shape_of_bole.dart';
+import 'package:forestor_2/src/data/slash_scent.dart';
 
 Map<String, dynamic> treeKey = {};
 getTreeKey() {
   treeKey.addAll(baseOfTree);
   treeKey.addAll(shapeOfBole);
   treeKey.addAll(barkSurfaceColor);
+  treeKey.addAll(slashScent);
   return treeKey;
 }
